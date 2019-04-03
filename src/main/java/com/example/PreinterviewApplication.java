7b8f72e4391fdf875e5230295580badc153babc4
Roberto Morales <roberto.x.morales@oracle.com>
2019-04-02 20:34:10 -0600

    --use RestTemplate more elegant

--- src/main/java/com/example/PreinterviewApplication.java
+++ src/main/java/com/example/PreinterviewApplication.java
@@ -1,44 +1,47 @@
 package com.example;
 
-import aj.org.objectweb.asm.TypeReference;
 import com.example.preinterview.entity.menuItems;
 import com.example.preinterview.repository.MenuRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 import org.springframework.boot.CommandLineRunner;
 import com.example.preinterview.entity.MenuList;
-import java.io.FileInputStream;
-import java.net.URL;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PreinterviewApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PreinterviewApplication.class, args);
-	}
-        
-       
-      	@Bean
-        public CommandLineRunner run(MenuRepo repo) throws Exception {
-            return new CommandLineRunner() {
-                @Override
-                public void run(String[] args) throws Exception {
-                    // read json and write to db
-                    ObjectMapper mapper = new ObjectMapper();
-                    URL url = new URL("https://davids-restaurant.herokuapp.com/menu_items.json");
-                    MenuList list = mapper.readValue(url.openStream(), MenuList.class);
-                    for (Iterator<menuItems> it = list.getMenu_items().iterator();
-                            it.hasNext();) {
-                        menuItems m = it.next();
-                        repo.save(m);                
-                    }
+    public static void main(String[] args) {
+        SpringApplication.run(PreinterviewApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public CommandLineRunner run(MenuRepo repo, RestTemplate restTemplate) throws Exception {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String[] args) throws Exception {
+                // read json and write to db
+                //ObjectMapper mapper = new ObjectMapper();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+                HttpEntity<MenuList> entity = new HttpEntity<>(headers);
+                MenuList list = restTemplate.exchange("https://davids-restaurant.herokuapp.com/menu_items.json", HttpMethod.GET, entity, MenuList.class).getBody();
+                for (menuItems m : list.getMenu_items()) {
+                    repo.save(m);
                 }
-            };	        
-        }
-        
+            }
+        };
+    }
+
 }
